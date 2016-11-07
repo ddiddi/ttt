fe7ed5e8b1153ebaab8a0218a3b90c7f539d6550
ddiddi <dhruv.diddi+1@gmail.com>
2016-11-07 01:29:44 -0600

    Print response user_name

--- server.py
+++ server.py
@@ -5,7 +5,19 @@ app = Flask(__name__)
 @app.route("/",methods=['POST','GET'])
 def game():
 	print("****Logging*****In main function")
-	print(request.form)
+	channel_id = request.form['channel_id']
+	token = request.form['token']
+	command = request.form['command']
+	team_id = request.form['team_id']
+	user_id = request.form['user_id']
+	response_url = request.form['response_url']
+	team_domain = request.form['team_domain']
+	channel_name = request.form['channel_name']
+	text = request.form['text']
+	user_name = request.form['user_name']
+	print user_name
+
+
 	return 'Test',200
 
 if __name__ == "__main__":
