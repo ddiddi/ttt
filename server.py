a67d1285784904a4c43e130fc4336ebf6cb13db5
ddiddi <dhruv.diddi+1@gmail.com>
2016-11-07 00:35:18 -0600

    Check response v4

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/",methods=['POST'])
 def game():
 	input_json = request.get_json(force=True)
-	return str(input_json),200
+	return 'Master',200
 #    return 'Test',200
 
 if __name__ == "__main__":
