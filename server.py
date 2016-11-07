51af5c223985807c50e6ddc885b442f95f2dc47e
ddiddi <dhruv.diddi+1@gmail.com>
2016-11-07 00:48:07 -0600

    Back to basics

--- server.py
+++ server.py
@@ -1,14 +1,10 @@
-from flask import Flask, request 
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/",methods=['POST','GET'])
 def game():
-	input_json = request.get_json(force=True)
-	print(input_json)
-	print("logging")
-	return 'Master',200
-#    return 'Test',200
+    return 'Test',200
 
 if __name__ == "__main__":
     app.run()
