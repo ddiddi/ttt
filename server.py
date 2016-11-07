4bd29b44f7c3dad05db4ff99539fbd6de32aff6e
ddiddi <dhruv.diddi+1@gmail.com>
2016-11-07 00:50:03 -0600

    Test logging

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 
 @app.route("/",methods=['POST','GET'])
 def game():
+	print("Test Printing Out AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa")
     return 'Test',200
 
 if __name__ == "__main__":
