0b58062dd8c0ff2ac61ce2cb0daa097d36d443a8
ddiddi <dhruv.diddi+1@gmail.com>
2016-11-07 01:09:55 -0600

    Add Requests get_data

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/",methods=['POST','GET'])
 def game():
 	print("****Logging*****In main function")
-	print(request.json)
+	print(request.get_data())
 	return 'Test',200
 
 if __name__ == "__main__":
