04978930d4ec7ee75f9e10833621c8e98113090c
ddiddi <dhruv.diddi+1@gmail.com>
2016-11-07 00:28:47 -0600

    Check response

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, request 
 
 app = Flask(__name__)
 
 @app.route("/",methods=['POST'])
 def game():
-    return 'Test',200
+	return request.get_json(force=True)
+#    return 'Test',200
 
 if __name__ == "__main__":
     app.run()
