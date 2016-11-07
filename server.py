b4c787ce308c32815e08076bca0cc4a76e6242c6
ddiddi <dhruv.diddi+1@gmail.com>
2016-11-07 00:21:45 -0600

    Edit for HTTP 200 Response

--- server.py
+++ server.py
@@ -2,9 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/",methods=['POST'])
 def game():
-    return 'Hello'
+    return 'Test',200
 
 if __name__ == "__main__":
-    app.run()   
+    app.run()
+
