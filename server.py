79f46929ccd3a0e2f923411418e66e2c35cb10ac
ddiddi <dhruv.diddi+1@gmail.com>
2016-11-09 01:14:37 -0600

    Add break bars new formatting

--- server.py
+++ server.py
@@ -91,12 +91,11 @@ class tictactoe:
 		}.get(position,-1)
 
 	def getBoard(self):
-		headerString = '\n -1 -2 -3  \n'
-		topLineString = '| '+self.boardValues[0]+' | '+self.boardValues[1]+' | '+self.boardValues[2]+' | a\n'
-		breakLineString = '|--+--+--| \n'
-		middleLineString = '| '+self.boardValues[3]+' | '+self.boardValues[4]+' | '+self.boardValues[5]+' | b\n'
-		bottomLineString = '| '+self.boardValues[6]+' | '+self.boardValues[7]+' | '+self.boardValues[8]+' | c\n'
-		outputString = headerString+topLineString+breakLineString+middleLineString+breakLineString+bottomLineString
+		headerString = ' 1     2     3\n'
+		topLineString = ' '+self.boardValues[0]+'     '+self.boardValues[1]+'     '+self.boardValues[2]+' a\n'
+		middleLineString = '| '+self.boardValues[3]+'     '+self.boardValues[4]+'     '+self.boardValues[5]+' b\n'
+		bottomLineString = '| '+self.boardValues[6]+'     '+self.boardValues[7]+'     '+self.boardValues[8]+' c\n'
+		outputString = headerString+topLineString+middleLineString+bottomLineString
 		return outputString
 
 	@property
